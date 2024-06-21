3c95d17c19ccdf690c636bf375b64a8f1bfd0d46
Thales Maia <thalesmaiaalexandre@gmail.com>
2024-06-21 09:02:02 -0300

    refactor: changing route path for better readability

--- src/main/java/org/example/simpleemail/email/EmailController.java
+++ src/main/java/org/example/simpleemail/email/EmailController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("email")
+@RequestMapping("send-email")
 public class EmailController {
 
     private final EmailService emailService;
